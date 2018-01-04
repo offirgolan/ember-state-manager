f9c6838f9b3d4dc20cc79483831fb1fe953fa244
Offir Golan <offirgolan@gmail.com>
2018-01-04 13:49:06 -0800

    Finish type based blueprint

--- blueprints/state/index.js
+++ blueprints/state/index.js
@@ -1,15 +1,45 @@
 /* eslint-env node */
+var path = require('path');
+
 module.exports = {
-  description: 'Create a new state definition',
+  description: 'Create a new state',
 
-  filesPath(options) {
-    var allowedTypes = ['object', 'array', 'buffered-object', 'buffered-array'];
-    var type = options.entity.options.type;
+  availableOptions: [
+    {
+      name: 'type',
+      type: ['object', 'array', 'buffered-object', 'buffered-array'],
+      default: 'object'
+    }
+  ],
 
-    if (allowedTypes.indexOf(type) > -1) {
-      return type + '-files';
+  install(options) {
+    this.options = options;
+    return this._super.install.apply(this, arguments);
+  },
+
+  uninstall(options) {
+    this.options = options;
+    return this._super.uninstall.apply(this, arguments);
+  },
+
+  filesPath() {
+    var dependencies = this.project.dependencies();
+    var type = this.options && this.options.type || 'object';
+
+    if (type === 'buffered-object' && !('ember-buffered-proxy' in dependencies)) {
+      this.ui.writeLine('Ember-buffered-proxy is not installed. Defaulting back to \'object\'...');
+      type = 'object';
+    } else if (type === 'buffered-array' && !('ember-buffered-array-proxy' in dependencies)) {
+      this.ui.writeLine('Ember-buffered-array-proxy is not installed. Defaulting back to \'array\'...');
+      type = 'array';
     }
 
-    return 'object-files';
-  }
+    return path.join(this.path, type + '-files');
+  },
+
+  // TODO: supportsAddon normally calls `files` which means that `filesPath`
+  // gets called before options get set. Likely needs to be fixed upstream.
+  supportsAddon() {
+    return false;
+  },
 };
